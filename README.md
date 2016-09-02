02e734bb440a66d203ea27c6248159292b191a41
robert <robert@monetsu.com>
2016-09-02 18:10:21 +0200

    Completed basic integration of full Cloudinary API support

--- README.md
+++ README.md
@@ -49,3 +49,14 @@ Images.attachSchema(new SimpleSchema({
 5. Run meteor
 
 `meteor --settings settings.json`
+
+6. Limitations
+
+Cloudinary supports many file formats including images, video, audio, and other documents.
+
+This link discusses it:
+https://support.cloudinary.com/hc/en-us/articles/204292392-Why-does-Cloudinary-reject-the-files-I-m-uploading-
+
+Based on that source, the below file formats are NOT supported by Cloudinary free/basic plans
+
+'action', 'apk', 'app', 'bat', 'bin', 'cmd', 'com', 'command', 'cpl', 'csh', 'exe', 'gadget', 'inf1', 'ins', 'inx', 'ipa', 'isu', 'job', 'jse', 'ksh', 'lnk', 'msc', 'msi', 'msp', 'mst', 'osx', 'out', 'paf', 'pif', 'prg', 'ps1', 'reg', 'rgs', 'run', 'sct', 'shb', 'shs', 'u3p', 'vb', 'vbe', 'vbs', 'vbscript', 'workflow', 'ws', 'wsf'.
--- autoform-cloudinary.js
+++ autoform-cloudinary.js
@@ -7,84 +7,163 @@ AutoForm.addInputType('cloudinary', {
 });
 
 Meteor.startup(function () {
-    Meteor.call('publicCredentials', function(err, res) {
-      if (res) {
-        $.cloudinary.config({
-          cloud_name: res.cloudName,
-          api_key: res.apiKey
-        });
-      } else {
-        $.cloudinary.config({
-          cloud_name: Meteor.settings.public.CLOUDINARY_CLOUD_NAME,
-          api_key: Meteor.settings.public.CLOUDINARY_API_KEY
-        });
-      }
-    });
+    // Meteor.call('publicCredentials', function(err, res) {
+    //   if (res) {
+    //     $.cloudinary.config({
+    //       cloud_name: res.cloudName,
+    //       api_key: res.apiKey
+    //     });
+    //   } else {
+    //     $.cloudinary.config({
+    //       cloud_name: Meteor.settings.public.CLOUDINARY_CLOUD_NAME,
+    //       api_key: Meteor.settings.public.CLOUDINARY_API_KEY
+    //     });
+    //   }
+    // });
 });
 
-var templates = ['afCloudinary', 'afCloudinary_bootstrap3'];
 
-_.each(templates, function (tmpl) {
-  Template[tmpl].onCreated(function () {
-    var self = this;
+Template.afCloudinary.onCreated(function () {
+  var self = this;
 
-    self.url = new ReactiveVar();
+  self.srcId = new ReactiveVar();
 
-    self.initialValueChecked = false;
-    self.checkInitialValue = function () {
-      Tracker.nonreactive(function () {
-        if (! self.initialValueChecked && ! self.url.get() && self.data.value) {
-          self.url.set(self.data.value);
-          self.initialValueChecked = true;
-        }
-      });
-    };
-  });
-
-  Template[tmpl].onRendered(function () {
-    var self = this;
-
-    Meteor.call('afCloudinarySign', function (err, res) {
-      if (err) {
-        return console.log(err);
+  self.initialValueChecked = false;
+  self.checkInitialValue = function () {
+    Tracker.nonreactive(function () {
+      if (! self.initialValueChecked && ! self.srcId.get() && self.data.value) {
+        self.srcId.set(self.data.value);
+        self.initialValueChecked = true;
       }
+    });
+  };
 
-      self.$('input[name=file]').cloudinary_fileupload({
-        formData: res
-      });
+  self.uploadFiles = function (file) {
+    // Here we can hand off uploading files..
+    // Also potentially allows for multi process support...
+
+    Cloudinary.upload(file,
+      {width: 1320, height: 960, crop: "limit"},
+      function (err, res) {
+      if (err) {
+        console.log("There was an error uploading to Cloudinary");
+        console.log(err);
+      } else {
+        console.log("Success uploading to Cloudinary!");
+        console.log(res);
+        self.srcId.set("v" + res.version + "/" + res.public_id);
+      };
     });
 
-    self.$('input[name=file]').on('fileuploaddone', function (e, data) {
-      self.url.set(data.result.secure_url);
-      Tracker.flush();
-    });
+  };
 
-    self.$(self.firstNode).closest('form').on('reset', function () {
-      self.url.set(null);
-    });
-  });
+});
 
-  Template[tmpl].helpers({
-    url: function () {
-      var t = Template.instance();
+Template.afCloudinary.onRendered(function () {
+  var self = this;
 
-      t.checkInitialValue();
-      return t.url.get();
-    },
-
-    accept: function () {
-      return this.atts.accept || 'image/*';
+  Meteor.call('afCloudinarySign', function (err, res) {
+    if (err) {
+      return console.log(err);
     }
+
+    // Initialize fields to be used based on server
+    // method call above
+    // self.$('input.cloudinary-fileupload[type=file]').cloudinary_fileupload({
+      // formData: res
+    // });
   });
 
-  Template[tmpl].events({
-    'click button': function (e, t) {
-      t.$('input[name=file]').click();
-    },
-
-    'click .js-remove': function (e, t) {
-      e.preventDefault();
-      t.url.set(null);
-    }
+  /*
+  self.$('input.cloudinary-fileupload[type=file]').on('fileuploaddone', function (e, data) {
+    // Seting data to id instead of url
+    console.log("checking results of upload");
+    console.log(data.result);
+    // self.srcId.set(data.result.secure_url);
+    self.srcId.set(data.result.public_id);
+    Tracker.flush();
   });
+*/
+
+  self.$("[data-nav='afDropUpload']").on('drop', function (e, data) {
+    // Just testing drag-n-drop implementation
+    // self.srcId.set(data.result.secure_url);
+    // Tracker.flush();
+  });
+
+  self.$(self.firstNode).closest('form').on('reset', function () {
+    self.srcId.set(null);
+  });
+});
+
+Template.afCloudinary.helpers({
+  fileImg: function () {
+    // This is to handle non-cloudinary images from FS.file
+
+  },
+  previewUrl: function () {
+    var t = Template.instance();
+    var srcId = t.srcId.get();
+    t.checkInitialValue();
+    // Force '.png' file type for previewing all files
+    var theUrl = $.cloudinary.url(srcId + ".png", {width: 480, height: 220, crop: 'limit'});
+    return theUrl;
+  },
+  srcId: function () {
+    var t = Template.instance();
+    t.checkInitialValue();
+    return t.srcId.get();
+  },
+
+  accept: function () {
+    return this.atts.accept || 'image/*';
+  },
+
+  label: function () {
+    return this.atts.label || 'Choose File';
+  },
+
+});
+
+Template.afCloudinary.events({
+
+ "dragover data-action[data-action='afDropUpload']": function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  },
+
+  "dragenter data-action[data-action='afDropUpload']": function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+  },
+
+  "drop data-action[data-action='afDropUpload']": function(e, t) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    var file = e.originalEvent.dataTransfer.files[0];
+    t.uploadFiles(file);
+
+    // return t.uploadFiles(new FS.File(e.originalEvent.dataTransfer.files[0]));
+  },
+
+  "click [data-action='afSelectFile']": function (e, t) {
+
+    $(e.currentTarget).siblings("input.cloudinary-fileupload[type=file]").click();
+
+  },
+
+  "click [data-action='afRemoveFile']": function(e, t) {
+    e.preventDefault();
+    t.srcId.set(null);
+  },
+
+  "change input.cloudinary-fileupload[type=file]": function(e, t, data) {
+
+    var file = e.currentTarget.files[0];
+    t.uploadFiles(file);
+
+    // return t.uploadFiles(new FS.File(data.files[0]));
+  },
+
 });
