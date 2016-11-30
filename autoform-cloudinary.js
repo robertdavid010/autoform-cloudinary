eeaa8416c516e17f9286f50152803c735c27db36
Robert David McLeod <robertdavid01@gmail.com>
2016-12-01 00:08:20 +0100

    remove feature fix, code cleanup

--- autoform-cloudinary.js
+++ autoform-cloudinary.js
@@ -1,3 +1,14 @@
+/*
+ * AutoForm Cloudinary handler package **********
+ *
+ * This package displays a file upload interface
+ * and handles user interaction and data handling
+ * of user selected files.
+ * TODO: Expand description and definitions
+ * https://github.com/blueimp/jQuery-File-Upload/wiki
+ *
+ */
+
 AutoForm.addInputType('cloudinary', {
   template: 'afCloudinary',
 
@@ -10,7 +21,6 @@ Template.afCloudinary.onCreated(function () {
   var self = this;
 
   self.srcId = new ReactiveVar();
-  self.upProg = new ReactiveDict();
 
   self.initialValueChecked = false;
   self.checkInitialValue = function () {
@@ -29,6 +39,9 @@ Template.afCloudinary.onCreated(function () {
         // NOTE: All of this is only to support other image sources,
         // by getting the image srcId from the form context.
         // as assumed naming convention for any data of a pic
+        // It helps with migration from existing img data store
+        // just put the old images in the template context with
+        // the autoform
         console.log("there was no field data");
 
         // Check the form data context for image items in form 'currentDoc'
@@ -63,93 +76,116 @@ Template.afCloudinary.onCreated(function () {
 
   });
 
-  self.uploadFiles = function (file) {
+  //  NOTE: Unused at the moment
+  // self.uploadFiles = function (file) {
     // Here we can hand off uploading files..
     // Also potentially allows for multi process support...
 
-    Cloudinary.upload(file,
-      {width: 1320, height: 960, crop: "limit"},
-      function (err, res) {
-      if (err) {
-        console.log("There was an error uploading to Cloudinary");
-        console.log(err);
-      } else {
-        console.log("Success uploading to Cloudinary!");
-        console.log(res);
-        // Prepare data for storing in database
-        self.srcId.set("v" + res.version + "/" + res.public_id);
-      };
-    });
+    // Cloudinary.upload(file,
+    //   {width: 1320, height: 960, crop: "limit"},
+    //   function (err, res) {
+    //   if (err) {
+    //     console.log("There was an error uploading to Cloudinary");
+    //     console.log(err);
+    //   } else {
+    //     console.log("Success uploading to Cloudinary!");
+    //     console.log(res);
+    //     // Prepare data for storing in database
+    //     self.srcId.set("v" + res.version + "/" + res.public_id);
+    //   };
+    // });
 
-  };
+  // };
 
 });
 
 Template.afCloudinary.onRendered(function () {
   var self = this;
 
+  var cdyElem = "input.cloudinary-fileupload[type=file]";
+  var progElem = ".progress-bar:not(.progress-bar-placeholder)";
+
   Meteor.call('afCloudinarySign', function (err, res) {
     if (err) {
       return console.log(err);
     } else {
       // Add result of server signing as config DOM object
-      // for full client-side uploading. Can include transformations
-      self.$('input.cloudinary-fileupload[type=file]').cloudinary_fileupload({
+      // for full client-side uploading (can include transformations)
+      self.$(cdyElem).cloudinary_fileupload({
         formData: res
       });
     }
 
   });
 
-  // Can we take this over for progress?
-  var cdyElem = 'input.cloudinary-fileupload[type=file]';
-  // self.$cdyElem.bind('fil')
-  // self.$('input.cloudinary-fileupload[type=file]').bind('fileuploadprogress', function(e, data) {
+  /* Progress Indicator *************************************
+   * NOTE: Go here for more info
+   * https://github.com/blueimp/jQuery-File-Upload/wiki/Options
+   *
+   */
+
+  // Initialize progress indicator element
   self.$(cdyElem).bind('fileuploadprogress', function(e, data) {
-    self.$('.progress > .progress-bar').css('width', Math.round((data.loaded * 100.0) / data.total) + '%');
+    self.$(progElem).css('width', Math.round((data.loaded * 100.0) / data.total) + '%');
+  });
+
+  // Fileupload hooks
+  self.$(cdyElem).bind('fileuploadadd', function(e, data) {
+    self.$("button[type=button]").hide();
+    self.$(".progress").show();
   });
 
   self.$(cdyElem).on('fileuploaddone', function (e, data) {
-    // Seting data to id instead of url
-    console.log("checking results of upload");
-    console.log(data.result);
-    // self.srcId.set(data.result.secure_url);
-    self.srcId.set(data.result.public_id);
-    Tracker.flush();
+    var res = data.result;
+    if (res) {
+
+      self.$(".progress").hide();
+      self.$("button[type=button]").show();
+      self.$(progElem).css("width", "5%");
+      self.$(".afCloudinary-Input").change();
+      self.srcId.set("v" + res.version + "/" + res.public_id);
+      Tracker.flush();
+
+    }
   });
 
-  // self.$("[data-nav='afDropUpload']").on('drop', function (e, data) {
-    // Just testing drag-n-drop implementation
-    // self.srcId.set(data.result.secure_url);
-    // Tracker.flush();
-  // });
-
+  // Reset reactiveVar on reset form
   self.$(self.firstNode).closest('form').on('reset', function () {
     self.srcId.set(null);
   });
 });
 
 Template.afCloudinary.helpers({
-  fileImg: function () {
-    // This is to handle non-cloudinary images from FS.file
-
-  },
-  cdyAtts: function () {
-    var dObj = {};
-    dObj["data-form-data"] = JSON.stringify({
-      "signature": "test"
-    });
-
-    return dObj;
-
-  },
   previewUrl: function () {
+
+    var conf = {};
     var t = Template.instance();
+    console.log("data context in previewUrl helper");
+    console.log(t);
+    var as = t.data.atts;
+    if (as.accept.toLowerCase().indexOf("pdf") != -1) {
+      conf.width = 480; conf.height = 270; conf.crop = "limit";
+    } else {
+      if (as["data-schema-key"].toLowerCase().indexOf("profile") != -1 || as.name.toLowerCase().indexOf("profile") != -1) {
+        conf.width = 270; conf.height = 270; conf.crop = "fill";
+      } else {
+        conf.width = 480; conf.height = 270; conf.crop = "fill";
+      }
+    }
+
     var srcId = t.srcId.get();
+    var theUrl;
     t.checkInitialValue();
-    var theUrl = $.cloudinary.url(srcId + ".png", {width: 480, height: 270, crop: 'fill'});
+    if (srcId) {
+      theUrl = $.cloudinary.url(srcId + ".png", conf);
+    } else if (as.accept.toLowerCase().indexOf("pdf") != -1) {
+      theUrl = "/img/icons/iconPDF_64.png";
+    } else {
+      theUrl = "/img/icons/iconCampaignImage_64.png";
+    }
     return theUrl;
   },
+
   srcId: function () {
     var t = Template.instance();
     t.checkInitialValue();
@@ -164,26 +200,11 @@ Template.afCloudinary.helpers({
     return this.atts.label || 'Choose File';
   },
 
-  origPic: function () {
-    // get the image from collection
-    // return Template.instance().data;
-    // WOW: 9 levels to get to the context we need...
-    // TODO: This is currently unused. Consider removing
-    var imgId;
-    var formDoc = Template.parentData(9).currentDoc;
-    if (formDoc && formDoc.picture) {
-      imgId = formDoc.picture;
-    } else if (formDoc && formDoc.profile && formDoc.profile.picture) {
-      imgId = formDoc.profile.picture;
-    };
-    return Template.parentData(2);
-  }
-
 });
 
 Template.afCloudinary.events({
 
- "dragover data-action[data-action='afDropUpload']": function(e) {
+  "dragover data-action[data-action='afDropUpload']": function(e) {
     e.stopPropagation();
     e.preventDefault();
   },
@@ -200,7 +221,6 @@ Template.afCloudinary.events({
     var file = e.originalEvent.dataTransfer.files[0];
     t.uploadFiles(file);
 
-    // return t.uploadFiles(new FS.File(e.originalEvent.dataTransfer.files[0]));
   },
 
   "click [data-action='afSelectFile']": function (e, t) {
@@ -214,19 +234,12 @@ Template.afCloudinary.events({
     t.srcId.set(null);
     // also need to clear the form...
     $(e.currentTarget).siblings("input.cloudinary-fileupload[type=file]").val("");
+    $(e.currentTarget).siblings("input.afCloudinary-Input").change();
   },
 
   "change input.cloudinary-fileupload[type=file]": function(e, t, data) {
-
-    // we should check to see if this is a remove
-    if ($(event.currentTarget).val()) {
-      console.log("we are removing the data")
-    } else {
-      var file = e.currentTarget.files[0];
-      t.uploadFiles(file);
-    }
-
     // return t.uploadFiles(new FS.File(data.files[0]));
+    // return t.uploadFiles(new FS.File(e.originalEvent.dataTransfer.files[0]));
   },
 
 });
